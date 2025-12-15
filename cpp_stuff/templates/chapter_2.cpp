767997f51a97986942f35a172f7a0b76a134c818
1y33 <catalin.milu.2004@gmail.com>
2025-12-15 23:36:30 +0200

    added more

--- cpp_stuff/templates/chapter_2.cpp
+++ cpp_stuff/templates/chapter_2.cpp
@@ -2,32 +2,74 @@
 
 // function tempalte
 template <typename T>
-T add(T const a, T const b){
-    return a + b ;
+T add(T const a, T const b)
+{
+    return a + b;
 }
 
-
 template <typename Input, typename Predicate>
-int count_if(Input start, Input end , Predicate p){
+int count_if(Input start, Input end, Predicate p)
+{
     int total = 0;
-    for (Input i = start ;  i!= end ; i ++){
+    for (Input i = start; i != end; i++)
+    {
         if (p(*i))
             total++;
     }
     return total;
 }
-int main(){
 
-    auto d = add<double>(41.2,1);
-    std::cout<<d;
+/// class template
+//
+template <typename T>
+class wrapper
+{
+    T value;
+
+public:
+    wrapper(T val) : value(val) {}
+};
+
+void use_wrapper(wrapper<int> *ptr) {}
+
+/// templated classes and also templated members
+
+template <typename T>
+class number
+{
+    T value;
+
+public:
+    number(T val) : value(val) {}
+
+    template <typename U>
+    U as()
+    {
+        return static_cast<U>(value);
+    }
+};
+
+int main()
+{
+
+    auto d = add<double>(41.2, 1);
+    std::cout << d;
 
     ///
-    int arr[]{1,2,3,4,4,4,4,4,4};
+    int arr[]{1, 2, 3, 4, 4, 4, 4, 4, 4};
     int odds = count_if(
-        std::begin(arr),std::end(arr),[](int const n){
-            return n%2 == 1;
-        }
-    );
-    std::cout<<"\nODDS "<<odds;
+        std::begin(arr), std::end(arr), [](int const n)
+        { return n % 2 == 1; });
+    std::cout << "\nODDS " << odds;
+
+    ///
+    wrapper<int> a(42);
+    use_wrapper(&a);
+
+    std::cout << "\n\n";
+
+    
+    number<double> test = 32.2;
+    std::cout << test.as<int>();
     return 0;
 }
---
+++ cpp_stuff/templates/chapter_2_notes.md
@@ -0,0 +1,7 @@
+that lambda shit
+class needs to have a definition we cant only declare it 
+
+-- 
+more types of templated parameters 
+the ones with 
+<template  ... T> Wtf
